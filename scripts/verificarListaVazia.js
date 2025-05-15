d7587cc3753a789c1c244b434aec3f34d289ba37
Thiago Machado <thiagomachado2525@gmail.com>
2025-05-15 19:18:21 -0300

    Update verificarListaVazia.js

--- scripts/verificarListaVazia.js
+++ scripts/verificarListaVazia.js
@@ -1,9 +1,9 @@
-// tirar a mensagem de "Não há nenhum item na lista no momento." assim que um item e adicionado na lista
+
 const mensagemListaVazia = document.querySelector('.mensagem-lista-vazia')
 
 function verificarListaVazia(listaDeCompras){
     const itensDaLista = listaDeCompras.querySelectorAll('li');
-    if (itensDaLista.length == 0) { //length significa a quantidade (apenas se utiliza em Listas)
+    if (itensDaLista.length == 0) { 
         mensagemListaVazia.style.display = 'block';
     } else {
         mensagemListaVazia.style.display = 'none';
